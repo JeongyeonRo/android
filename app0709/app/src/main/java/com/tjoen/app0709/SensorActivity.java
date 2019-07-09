4a54c31a971fa78d2f809423e6c08f3067b8b59a
JeongyeonRo <rgo0517@gmail.com>
2019-07-09 17:14:34 +0900

    update
    update sms service

---
+++ app0709/app/src/main/java/com/tjoen/app0709/SensorActivity.java
@@ -0,0 +1,41 @@
+package com.tjoen.app0709;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class SensorActivity extends AppCompatActivity {
+
+    ListView listView;
+    List<Sensor> list;
+    SensorListAdapter adapter;
+
+    SensorManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        //센서 목록 가져오기
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        list = manager.getSensorList(Sensor.TYPE_ALL);
+        Log.e("데이터목록", list.toString());
+        //가져온 list로 adapter 만들기
+        adapter = new SensorListAdapter(
+                SensorActivity.this,
+                R.layout.customcell, list);
+        //ListView를 만들고 연결하기
+        listView = (ListView) findViewById(R.id.sensorlist);
+
+        listView.setAdapter(adapter);
+    }
+}
+
